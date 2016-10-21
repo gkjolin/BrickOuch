183c3858fec5c180b3effe1d7d701d9f6aedf4bc
Fernando Gorodscy <fegorodscy@gmail.com>
2016-10-21 19:01:11 -0200

    Change the tweak factor do send the ball down rather than up, so that it does not get locked in the ceiling

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -30,7 +30,7 @@ public class Ball : MonoBehaviour {
 	void OnCollisionEnter2D (Collision2D collision) {
 		// Ball does not trigger sound when brick is destoyed.
 		// Not 100% sure why, possibly because brick isn't there.
-		Vector2 tweak = new Vector2 (Random.Range(0f, 0.2f), Random.Range(0f, 0.2f));
+		Vector2 tweak = new Vector2 (Random.Range(0f, -0.2f), Random.Range(0f, -0.2f));
 		
 		if (hasStarted) {	
 			GetComponent<AudioSource>().Play();

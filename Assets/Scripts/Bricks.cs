ca19825d8ddfa654457554ade802e4c88668d542
Guto <gutomarzagao@gmail.com>
2016-10-23 00:43:10 -0200

    Randomize brick color

--- Assets/Scripts/Bricks.cs
+++ Assets/Scripts/Bricks.cs
@@ -1,10 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Bricks : MonoBehaviour {
 
 	private const int MaxBricks = 50;
-	string path = "Prefabs/yellow";
+	private Dictionary<BrickType, string> brickPaths = new Dictionary<BrickType, string> () {
+		{ BrickType.Yellow, "Prefabs/yellow" },
+		{ BrickType.Green, "Prefabs/green" },
+		{ BrickType.Red, "Prefabs/red" }
+	};
+
 	GameObject[,] bricks = new GameObject[6, 16];
 
 	// Use this for initialization
@@ -16,18 +22,24 @@ public class Bricks : MonoBehaviour {
 	
 	public void CreateRandomBrick() {
 		if (Brick.breakableCount < MaxBricks) {
+			BrickType type = (BrickType)Random.Range (0, 3);
 			int posX = Random.Range (0, 6);
 			int posY = Random.Range (0, 16);
-			CreateBrick (posX, posY);
+
+			CreateBrick (type, posX, posY);
 		}
 	}
 
-	private void CreateBrick(int posX, int posY) {
+	private void CreateBrick(BrickType type, int posX, int posY) {
 		if (bricks [posX, posY] == null) {
-			bricks [posX, posY] = Instantiate (Resources.Load (path)) as GameObject;
+			bricks [posX, posY] = Instantiate (Resources.Load (brickPaths[type])) as GameObject;
 			
 			Vector2 startPos = new Vector2 (150 * posX, 50 * posY + 800);
 			bricks [posX, posY].transform.position = startPos;
 		}
 	}
+}
+
+public enum BrickType {
+	Green, Yellow, Red
 }

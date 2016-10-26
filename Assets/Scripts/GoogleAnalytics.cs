a9bc2d1d59a6a24a34e17f6701d9fd5d268edde8
Guto <gutomarzagao@gmail.com>
2016-10-26 20:57:23 -0200

    Fix Google Analytics again

---
+++ Assets/Scripts/GoogleAnalytics.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using SA.Analytics.Google;
+
+public class GoogleAnalytics : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Manager.StartTracking ();
+	}
+	
+}

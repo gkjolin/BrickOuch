0b7d1a6f64df0183a5474b10f6e885f130633d37
Fabio Beider <fabeider@gmail.com>
2016-11-01 14:38:01 -0200

    Track Google Analytics events for touch or accelerometer game modes

--- Assets/Scripts/HowToPlayController.cs
+++ Assets/Scripts/HowToPlayController.cs
@@ -1,15 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
+using SA.Analytics.Google;
 
 public class HowToPlayController : MonoBehaviour {
 
 	public void SetPlayWithMouse()
 	{
 		PlayerPrefsManager.SetControllerMode(Constants.HowToPlayModes.PlayWithMouse);
+
+
+		HitAnalyticsEvent("touch");
 	}
 
 	public void SetPlayWithAccelerometer()
 	{
 		PlayerPrefsManager.SetControllerMode(Constants.HowToPlayModes.PlayWithAccelerometer);
+
+		HitAnalyticsEvent("accelerometer");
 	}
+
+	void HitAnalyticsEvent(string eventAction)
+	{
+		Manager.Client.CreateHit(HitType.EVENT);
+		Manager.Client.SetEventCategory("howtoplay");
+		Manager.Client.SetEventAction(eventAction);
+		Manager.Client.Send();
+	}
+		
 }

d3b32e414a20f077bc7d232532c9be7a0005cec2
Fabio Beider <fabeider@gmail.com>
2016-11-03 15:45:04 -0200

    Implement game Pause feature

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -2,14 +2,36 @@
 using UnityEngine.SceneManagement;
 using System.Collections;
 
-public class LevelManager : MonoBehaviour {
+public class LevelManager : MonoBehaviour
+{
 
-	public void LoadScene(string name){
+	void Update ()
+	{
+		/*if (Time.timeScale == 0) {
+			Time.timeScale = 1f;
+		} else {
+			Time.timeScale = 0;
+		}*/
+	}
+
+	public void LoadScene (string name)
+	{
 		Brick.breakableCount = 0;
 		SceneManager.LoadScene (name);
 	}
 
-	public void QuitRequest(){
+	public void QuitRequest ()
+	{
 		Application.Quit ();
 	}
+
+	public void PauseGame(){
+		if (Time.timeScale == 0) {
+			Time.timeScale = 1f;
+		} else {
+			Time.timeScale = 0;
+		}
+	}
+
+
 }

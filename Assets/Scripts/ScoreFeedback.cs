07208d1dbd36844e926d89968e931f6cad59e544
Fabio Beider <fabeider@gmail.com>
2016-11-09 18:12:49 -0200

    Redesign Start Screen

--- Assets/Scripts/ScoreFeedback.cs
+++ Assets/Scripts/ScoreFeedback.cs
@@ -4,14 +4,16 @@ using System.Collections;
 
 public class ScoreFeedback : MonoBehaviour {
 
+	public Text bestScore;
+	public Text lastScore;
+
 	// Use this for initialization
 	void Start () {
 		GameObject scoreObj = GameObject.Find ("Score");
 		Score score = scoreObj.GetComponent<Score> ();
 
-		Text text = this.GetComponent<Text> ();
-		text.text = "Score: " + score.GetScore ();
-		text.text += "\nHighest Score: " + score.GetHighestScore();
+		lastScore.text += score.GetScore ();
+		bestScore.text += score.GetHighestScore();
 
 		Destroy (scoreObj.transform.root.gameObject);
 	}

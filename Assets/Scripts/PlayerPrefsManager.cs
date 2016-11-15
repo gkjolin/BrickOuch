845e1f0bf42c722b314311e8a140615202b1de85
Fernando Gorodscy <fegorodscy@gmail.com>
2016-11-15 13:41:59 -0200

    Remove Debug

--- Assets/Scripts/PlayerPrefsManager.cs
+++ Assets/Scripts/PlayerPrefsManager.cs
@@ -31,7 +31,6 @@ public static class PlayerPrefsManager {
 		if (volume >= 0 && volume <= 1)
 		{
 			PlayerPrefs.SetFloat(SOUNDS_VOLUME_KEY, volume);
-			Debug.LogError(PlayerPrefs.GetFloat(SOUNDS_VOLUME_KEY));
 		}
 		else
 		{

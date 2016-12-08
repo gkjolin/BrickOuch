45b52dda77c9470c2629383488e89aba114508ec
Fabio Beider <fabeider@gmail.com>
2016-12-08 21:37:58 -0200

    Verify if logged in before post score

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -3,51 +3,54 @@ using UnityEngine.UI;
 using System.Collections;
 using Facebook.Unity;
 
-public class Score : MonoBehaviour {
+public class Score : MonoBehaviour
+{
 
 	private int score = 0;
 	private int highestScore = 0;
 	private Text scoreText;
 
 	// Use this for initialization
-	void Start () {
-		highestScore = PlayerPrefsManager.GetHighestScore();
+	void Start ()
+	{
+		highestScore = PlayerPrefsManager.GetHighestScore ();
 		scoreText = GetComponent<Text> ();
 		scoreText.text = string.Format ("{0}", score);
 	}
 
-	public void AddScore(int points = 10) {
+	public void AddScore (int points = 10)
+	{
 		score += points;
 		scoreText.text = string.Format ("{0}", score);
 	}
 
-	public int GetScore() {
+	public int GetScore ()
+	{
 		return score;
 	}
 
-	public int GetHighestScore()
+	public int GetHighestScore ()
 	{
-		UpdateHighestScore();
+		UpdateHighestScore ();
 
 		return highestScore;
 	}
 
-	public void UpdateHighestScore()
+	public void UpdateHighestScore ()
 	{
-		PlayerPrefsManager.SetLastScore(score);
+		PlayerPrefsManager.SetLastScore (score);
 
-		if (score > highestScore)
-		{
+		if (score > highestScore) {
 			highestScore = score;
-			PlayerPrefsManager.SetHighestScore(score);
-			if(FB.IsLoggedIn){
-				PlayfabAccess.PostScore(score);
-				}
+			PlayerPrefsManager.SetHighestScore (score);
+			if (FB.IsLoggedIn) {
+				PlayfabAccess.PostScore (score);
+			}
 		}
 	}
 
-	void OnDisable()
+	void OnDisable ()
 	{
-		UpdateHighestScore();
-    }
+		UpdateHighestScore ();
+	}
 }

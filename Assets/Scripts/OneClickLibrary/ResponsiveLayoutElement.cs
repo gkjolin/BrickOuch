bb2018c6336fdd89f6b454411c3ce7f3e668538d
Guto <gutomarzagao@gmail.com>
2016-12-15 01:28:12 -0200

    Responsive scroll view

---
+++ Assets/Scripts/OneClickLibrary/ResponsiveLayoutElement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ResponsiveLayoutElement : MonoBehaviour {
+
+	public RectTransform reference;
+	public Vector2 percentage;
+	public bool switchAxis;
+
+	void Update () {
+		var size = reference.rect.size;
+		if (switchAxis) {
+			size.Set (size.y, size.x);
+		}
+
+		size.Scale (percentage);
+
+		var layoutElement = GetComponent<LayoutElement> ();
+		if (layoutElement == null) {
+			layoutElement = gameObject.AddComponent <LayoutElement> ();
+		}
+
+		if (percentage.x != 0) {
+			layoutElement.preferredWidth = size.x;
+		}
+
+		if (percentage.y != 0) {
+			layoutElement.preferredHeight = size.y;
+		}
+	}
+}
---
+++ Assets/Scripts/OneClickLibrary/ScrollableSettings.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScrollableSettings : MonoBehaviour {
+
+	public int visibleCount;
+
+	// Use this for initialization
+	void Start () {
+		var scroll = GetComponent <ScrollRect> ();
+		var scrollRect = scroll.GetComponent<RectTransform> ();
+		var content = scroll.content;
+
+		if (content.childCount < visibleCount) {
+			var minY = scrollRect.anchorMin.y;
+			var maxY = scrollRect.anchorMax.y;
+
+			var scrollAreaSize = maxY - minY;
+			var newScrollAreaSize = scrollAreaSize * (float)content.childCount / (float)visibleCount;
+			var newMinY = maxY - newScrollAreaSize;
+
+			scrollRect.anchorMin = new Vector2 (scrollRect.anchorMin.x, newMinY);
+			scrollRect.sizeDelta = Vector2.zero;
+		} else {
+			var minY = 1f - (float)content.childCount / (float)visibleCount;
+			content.anchorMin = new Vector2 (0, minY);
+			content.sizeDelta = Vector2.zero;
+		}
+	}
+}
